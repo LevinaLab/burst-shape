8deba34fcf0d486da3c99b1bd2f2e384dc462ea2
schaefertim <tim@gruber-schaefer.de>
2024-10-08 09:49:30 +0200

    plot example bursts

---
+++ scripts/103_plot_example_bursts.py
@@ -0,0 +1,75 @@
+import os
+
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import fcluster
+
+from src.folders import get_results_folder
+from src.persistence import load_df_bursts, load_burst_matrix
+
+burst_extraction_params = "burst_n_bins_50_normalization_integral_min_length_30_smoothing_kernel_4"
+# load bursts df
+df_bursts = load_df_bursts(burst_extraction_params)
+burst_matrix = load_burst_matrix(burst_extraction_params)
+# load labels
+linkage_file = os.path.join(
+    get_results_folder(),
+    burst_extraction_params,
+    "agglomerating_clustering_linkage_complete_n_bursts_None",
+    "linkage.npy",
+)
+linkage = np.load(linkage_file)
+
+n_clusters = 9
+palette = sns.color_palette(n_colors=n_clusters)
+labels = fcluster(linkage, t=n_clusters, criterion="maxclust")
+df_bursts["cluster"] = labels
+
+#####################################
+# Choose a cluster to plot
+i_cluster = 2
+n_bursts_to_plot = 1  # number of bursts to plot
+index_to_plot = None  # None plots a random one
+plot_average = True  # plot average burst
+
+# plot bursts
+fig, ax = plt.subplots()
+sns.despine()
+df_bursts_i = df_bursts[df_bursts["cluster"] == i_cluster]
+n_bursts_i = df_bursts_i.shape[0]
+
+if plot_average:
+    ax.plot(
+        burst_matrix[df_bursts["cluster"] == i_cluster].mean(axis=0),
+        color=palette[i_cluster - 1],
+        alpha=1,
+        linewidth=2,
+    )
+
+if index_to_plot is None:
+    index_to_plot = np.random.randint(0, n_bursts_i, n_bursts_to_plot)
+# if not iterable make a list
+if not hasattr(index_to_plot, "__iter__"):
+    index_to_plot = [index_to_plot]
+
+for idx in index_to_plot:
+    print(df_bursts_i.iloc[idx])
+    bins = np.linspace(
+        0, df_bursts_i.iloc[idx]["time_orig"], 51, endpoint=True
+    )
+    bins_mid = (bins[1:] + bins[:-1]) / 2
+    ax.plot(
+        bins_mid,
+        df_bursts_i.iloc[idx]["burst"],
+        color=palette[i_cluster - 1],
+        alpha=1,
+        linewidth=1,
+    )
+ax.set_ylabel("Rate [Hz]")
+ax.set_xlabel("Time [ms]")
+fig.show()
+
+# %% count labels
+print("Number of bursts per cluster")
+print(df_bursts["cluster"].value_counts())
